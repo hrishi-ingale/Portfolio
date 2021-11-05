694100acba106e4326de50d82f4e51c8c2f82c99
Hrishikesh Ingale <70112755+hrishi-ingale@users.noreply.github.com>
2021-11-04 18:42:12 -0700

    calcGUI

---
+++ calcGUI/Main.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        JavaCalculator calc = new JavaCalculator();
+        calc.pack();
+        calc.setLocationRelativeTo(null);
+                calc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        calc.setVisible(true);
+    }
+}
